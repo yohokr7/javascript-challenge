1ecf15004066c9a2da4f4ece10202a5f62662ddb
yohokr7 <62631358+yohokr7@users.noreply.github.com>
2020-08-29 13:38:52 -0700

    Add files via upload

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -3,13 +3,31 @@ var tableData = data;
 
 // YOUR CODE HERE!
 
-// Get a reference to the table body
 var tbody = d3.select("tbody");
 
-data.forEach((alienReport) => {
-    var row = tbody.append("tr");
-    Object.entries(alienReport).forEach(([key, value]) => {
-      var cell = tbody.append("td");
-      cell.text(value);
+function createTable(data){
+
+    data.forEach((alienReport) => {
+        var row = tbody.append("tr");
+        Object.entries(alienReport).forEach(([key, value]) => {
+        var cell = tbody.append("td");
+        cell.text(value);
+        });
     });
-  });
+};
+
+function searchClick() {
+    tbody.html("");
+    
+    const date = d3.select("#datetime").property("value");
+    let searchedData = tableData;
+    if (date) {
+        searchedData = searchedData.filter(row => row.datetime === date);
+    }
+    createTable(searchedData);
+}
+
+// Attach an event to listen for the form button
+d3.selectAll("#filter-btn").on("click", searchClick);
+
+createTable(tableData);
