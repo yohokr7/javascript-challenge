6359e70766d3997ba266b289cf1f728e7b28bb90
yohokr7 <62631358+yohokr7@users.noreply.github.com>
2020-08-28 22:51:28 -0700

    Added Table Creation

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -2,3 +2,14 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+
+// Get a reference to the table body
+var tbody = d3.select("tbody");
+
+data.forEach((alienReport) => {
+    var row = tbody.append("tr");
+    Object.entries(alienReport).forEach(([key, value]) => {
+      var cell = tbody.append("td");
+      cell.text(value);
+    });
+  });
